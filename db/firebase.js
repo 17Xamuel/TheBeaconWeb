eb3de5903e092d773eb88790e26938a0d995d069
XamuelUG <samweko@gmail.com>
2021-05-15 12:48:02 +0300

    started server

---
+++ db/firebase.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./thebeaconlira-firebase-adminsdk-25gf9-5b6c539cd8.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const firestore = admin.firestore();
+
+export default firestore;
