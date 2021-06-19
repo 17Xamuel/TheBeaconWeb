615d621d1c4e4ec1ac274dd71713029e0fc7526a
XamuelUG <samweko@gmail.com>
2021-06-19 18:57:34 +0300

    change

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -5,21 +5,12 @@ const d = document;
 const dq = d.querySelector;
 
 async function sbt(f, url, type = "POST") {
-  let res = "empty";
   let _fcontent = {};
   f = new fd(f);
   f.forEach((value, key) => {
     _fcontent[key] = value;
   });
-  //   let req = new request();
-  //   req.open(type, url, true);
-  //   req.setRequestHeader("Content-type", "application/json");
-  //   req.send(_fcontentJson);
-  //   req.onreadystatechange = () => {
-  //     if (req.status == 200 && req.readyState == 4) {
-  //       res = req.responseText;
-  //     }
-  //   };
+
   const _res = await fetch(url, {
     method: type,
     headers: {
